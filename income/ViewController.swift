1c1268a24397db7c03980f90dc60986ea3d9f08b
Mikkel Kim <wjsekfghks@gmail.com>
2017-01-09 19:55:09 -0600

    Add basic interface

--- income/ViewController.swift
+++ income/ViewController.swift
@@ -1,15 +1,21 @@
 //
 //  ViewController.swift
-//  income
+//  CalculateIncome
 //
 //  Created by Mikkel C. Kim on 1/9/17.
-//  Copyright © 2017 foodforthought. All rights reserved.
+//  Copyright © 2017 Mikkel C. Kim. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var grossIncome: UITextField!
+    @IBOutlet weak var deduction: UITextField!
+    @IBOutlet weak var ira: UITextField!
+    @IBOutlet weak var healthcare: UITextField!
+    @IBOutlet weak var netIncome: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateIncome(_ sender: AnyObject) {
+        grossIncome.resignFirstResponder()
+        netIncome.text = grossIncome.text
+    }
 
 }
 
