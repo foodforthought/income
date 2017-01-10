9559570ea7419e8a6d5de18bceaa04f1758085cb
Mikkel Kim <wjsekfghks@gmail.com>
2017-01-09 19:50:07 -0600

    Initial Commit

---
+++ income/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  income
+//
+//  Created by Mikkel C. Kim on 1/9/17.
+//  Copyright © 2017 foodforthought. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
